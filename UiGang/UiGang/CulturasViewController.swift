163398193841bc455ecdb10128d7dcd0d51ff70a
Caio Zavarezzi balieiro <czaio11@10-40-48-90.0200.ecps.br.iosda.org>
2023-07-04 09:43:36 -0300

    storyboard organization

---
+++ UiGang/UiGang/CulturasViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CulturasViewController.swift
+//  UiGang
+//
+//  Created by Caio Zavarezzi balieiro on 04/07/23.
+//
+
+import UIKit
+
+class CulturasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
