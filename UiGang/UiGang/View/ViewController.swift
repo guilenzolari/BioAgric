59f307c1505e7430dea0c01402c2a0e952761018
Guilherme Lenzolari <103944988+guilenzolari@users.noreply.github.com>
2023-07-05 11:23:00 -0300

    api-culturas

--- UiGang/UiGang/View/ViewController.swift
+++ UiGang/UiGang/View/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
   var controller: PragasController = PragasController()
   var arrayPragas:[String] = []
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     self.pragasTableView.delegate = self
