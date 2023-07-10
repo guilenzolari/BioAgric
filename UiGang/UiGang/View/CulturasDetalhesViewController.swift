e9a8a375b2e1b86ef0e2370a3110258fbf434fd0
Guilherme Lenzolari <103944988+guilenzolari@users.noreply.github.com>
2023-07-10 12:06:30 -0300

    cell xib preliminar

--- UiGang/UiGang/View/CulturasDetalhesViewController.swift
+++ UiGang/UiGang/View/CulturasDetalhesViewController.swift
@@ -14,18 +14,29 @@ class CulturasDetalhesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        inoculantesTableView.dataSource = self
+        inoculantesTableView.delegate = self
+        inoculantesTableView.register(UINib(nibName: "PictureCell", bundle: nil), forCellReuseIdentifier: "PictureCell")
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension CulturasDetalhesViewController: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = (tableView.dequeueReusableCell(withIdentifier: "PictureCell") as? PictureCell)!
+        
+        cell.tableImage.image = UIImage(named: "Image")
+        cell.tableTitle.text = "Nome Comum"
+        cell.tableSubTitle.text = "Nome Científico"
+        
+        return cell
+    }
 }
---
+++ UiGang/UiGang/View/PictureCellView.swift
@@ -0,0 +1,16 @@
+//
+//  PictureCellView.swift
+//  UiGang
+//
+//  Created by Guilherme Ferreira Lenzolari on 10/07/23.
+//
+
+import Foundation
+import UIKit
+
+class PictureCell: UITableViewCell{
+    
+    @IBOutlet weak var tableImage: UIImageView!
+    @IBOutlet weak var tableTitle: UILabel!
+    @IBOutlet weak var tableSubTitle: UILabel!
+}
